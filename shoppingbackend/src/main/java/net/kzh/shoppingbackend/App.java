3c100bf5d9759416749446fcced1c3ecc0e720a4
Akhilesh Nambiar <akkhilnambiar@gmail.com>
2018-11-07 21:28:38 +0000

    Project setup completed

---
+++ shoppingbackend/src/main/java/net/kzh/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.kzh.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
